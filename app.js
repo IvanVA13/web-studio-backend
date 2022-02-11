ee7f91aa0014521da0c014a8d88753436432b3be
Ivan Volobuev <ivannnn2425@gmail.com>
2022-02-12 00:13:50 +0200

    fix bag with cors

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ const {
 const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.use(helmet());
 app.use(cors());
+app.use(helmet());
 app.use(logger(formatsLogger));
 app.use(express.json({ limit: 5000 }));
 app.use(boolParser());
