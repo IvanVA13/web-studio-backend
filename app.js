d6ad3ed6c7b12f14fd0116e5e56c061571b1e08f
Ivan Volobuev <ivannnn2425@gmail.com>
2022-02-11 23:08:23 +0200

    fix bag with cors

--- app.js
+++ app.js
@@ -21,12 +21,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 app.options('*', cors());
 app.use(helmet());
 app.use(logger(formatsLogger));
-app.use(
-  cors({
-    origin: BASE_URL_FRONT,
-    credentials: true,
-  }),
-);
+app.use(cors());
 app.use(express.json({ limit: 5000 }));
 app.use(boolParser());
 app.use('/api', rateLimit(reqLimiterAPI));
